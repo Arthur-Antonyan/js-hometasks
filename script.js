16097ebdf0977dcae40e3ef289821ada371f10ad
Arthur-Antonyan <arturantonyan95@gmail.com>
2021-12-08 12:20:31 +0400

    changed callbacks

--- script.js
+++ script.js
@@ -1,53 +1,52 @@
-
-/////////// find
+/////////////////////////find
 let arr1 = [1,2,3,4,500]
-function find(arr,func){
+function find(arr,callback){
    for(let item of arr){
-       if (func(item)==arr[item]){
-           return func(item)
+       if (callback(arr[item])){
+           return arr[item]
        }
    }
 }
 
-let check1 = find(arr1,()=>4)
+let check1 = find(arr1,item=>item>3)
 console.log(check1)
 
 
-/////////// findIndex
+///////// findIndex
 
 let arr2 = [1,2,3,4,500]
-function findIndex(arr,func){
+function findIndex(arr,callback){
    for(let i = 0;i<arr.length;i++){
-       if (func(i)==arr[i]){
+       if (callback(arr[i])){
            return i
        }
-   }
+   }return -1
 }
 
-let check2 = findIndex(arr2,()=>2)
+let check2 = findIndex(arr2,item=>item===800)
 console.log(check2)
 
 /////////////////////some
 
 let arr3 = [1,2,3,4,500]
-function some(arr,func){
+function some(arr,callback){
    for(let i = 0;i<arr.length;i++){
-       if (func(i)==arr[i]){
+       if (callback(arr[i])){
            return true
        }
    }return false
 }
 
-let check3 = some(arr3,()=>3)
+let check3 = some(arr3,item=>item%2===0)
 console.log(check3)
 
 
 /////////////////////every
 
-let arr4 = [3,3,3,3,3,5]
-function every(arr,func){
+let arr4 = [4,5,6,7,8]
+function every(arr,callback){
    for(let i = 0;i<arr.length;i++){
-       if (func(i)!=arr[i]){
+       if (!callback(arr[i])){
 
         return false
        }
@@ -55,5 +54,6 @@ function every(arr,func){
    } return true 
 }
 
-let check4 = every(arr4,()=>3)
+let check4 = every(arr4,item=>item>3)
 console.log(check4)
+
